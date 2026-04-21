7bc8893900e854a56a234c0d29bee09f661db16a
luisjimenezgx@gmai.com <luisjimenezgx@gmail.com>
2026-04-21 15:35:08 -0400

    feat(banco): add crearCuenta method and update cuenta constructor

--- src/com/krakedev/financiero/test/TestCuenta.java
+++ src/com/krakedev/financiero/test/TestCuenta.java
@@ -7,7 +7,7 @@ public class TestCuenta {
 
 	public static void main(String[] args) {
 		
-		Cuenta cuenta1 = new Cuenta("22078222", 3000, "C");
+		Cuenta cuenta1 = new Cuenta("22078222");
 		cuenta1.imprimir();
 		
 		Cliente cliente1 = new Cliente("175612822","Luis", "Aristeguieta");
